c99221b94798801097316b019682a4d299d44ead
Jake Nixon <jake@jakenixon.com>
2024-08-09 11:50:14 -0600

    added vim settings lua file

---
+++ .config/nvim/lua/set.lua
@@ -0,0 +1,33 @@
+-- tabulation
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+
+-- disable mouse
+vim.opt.mouse = ""
+
+--line numbers
+vim.opt.nu = true
+vim.opt.ruler = true
+vim.opt.relativenumber = true
+
+-- line highlighting
+vim.opt.cursorline = true
+
+-- term colors
+vim.opt.termguicolors = true
+
+-- auto scroll
+vim.opt.scrolloff = 6
+
+-- always show gutter
+vim.opt.signcolumn = "yes"
+
+-- easy case insensitivity in commands and search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- nerd font
+vim.g.have_nerd_font = true
